8c9dd086347d35d5cbedc366e299e3084a76ea09
asjensen14 <56127260+asjens14@users.noreply.github.com>
2026-01-07 14:54:01 -0700

    submit question command

---
+++ src/commands/util/submitQuestion.js
@@ -0,0 +1,29 @@
+import { MessageFlags, SlashCommandBuilder } from "discord.js";
+import { handleQuestionSubmit } from "../../utils/handleQuestionSubmit.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("dq")
+    .addStringOption((option) =>
+      option
+        .setName("question")
+        .setDescription("Submit your daily question")
+        .setRequired(true)
+    )
+    .setDescription("Submit a question for the daily question prompt."),
+  async execute(interaction) {
+    await interaction.reply({
+      content: "Question Submitted",
+      flags: MessageFlags.Ephemeral,
+    });
+    const displayName =
+      interaction.member?.displayName || interaction.user.username;
+
+    await handleQuestionSubmit(
+      interaction.user.avatarURL(),
+      displayName,
+      interaction.options.getString("question")
+    );
+    // await sendDailyQuestion(); //replace with actual handling
+  },
+};
