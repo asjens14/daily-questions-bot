db84f91c8049eb4ffc1c334cf5d81c5ec24f8a61
asjensen14 <56127260+asjens14@users.noreply.github.com>
2026-01-09 17:10:37 -0700

    add function for question approval by mods

--- src/utils/sendToMods.js
+++ src/utils/sendToMods.js
@@ -1,1 +1,54 @@
-export async function sendToMods(avatar, nickname, questionText) {}
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+} from "discord.js";
+
+import { saveQuestion } from "./saveQuestion.js";
+
+export async function sendToMods(channel, avatar, nickname, questionText) {
+  const embed = new EmbedBuilder()
+    .setColor(413059)
+    .setTitle("New Daily Question Submission")
+    .setAuthor({ name: nickname, iconURL: avatar })
+    .setDescription(questionText);
+
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId("approve")
+      .setLabel("Approve")
+      .setStyle(ButtonStyle.Success),
+    new ButtonBuilder()
+      .setCustomId("reject")
+      .setLabel("Reject")
+      .setStyle(ButtonStyle.Danger)
+  );
+
+  const message = await channel.send({
+    embeds: [embed],
+    components: [row],
+    avatarURL:
+      "https://cdn.discordapp.com/avatars/1375237397578649732/9f00a4299abce52f6da33c3a84aaadce.webp?size=128",
+    username: "Daily Question Bot",
+  });
+
+  const collector = message.createMessageComponentCollector();
+
+  collector.on("collect", async (interaction) => {
+    if (interaction.customId === "approve") {
+      await interaction.update({
+        content: "✅ Approved",
+        embeds: [embed],
+        components: [],
+      });
+      await saveQuestion(avatar, nickname, questionText);
+    } else if (interaction.customId === "reject") {
+      await interaction.update({
+        content: "❌ Rejected",
+        embeds: [embed],
+        components: [],
+      });
+    }
+  });
+}
