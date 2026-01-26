be986ef8c5e78ecd2d8241fb50c3646e12a429ff
asjensen14 <56127260+asjens14@users.noreply.github.com>
2026-01-26 15:24:00 -0700

    add guild member intent

--- src/bot.js
+++ src/bot.js
@@ -19,6 +19,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
   ],
   partials: [Partials.Channel],
 });
