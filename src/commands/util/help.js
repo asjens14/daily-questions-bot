51d11d0b8df87ab909c348405fc5ab3af504edb7
asjensen14 <56127260+asjens14@users.noreply.github.com>
2026-09-02 23:46:44 -0600

    add help command

---
+++ src/commands/util/help.js
@@ -0,0 +1,20 @@
+import { MessageFlags, SlashCommandBuilder } from "discord.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Show available commands."),
+  async execute(interaction) {
+    const embed = {
+      title: "Help",
+      description: "List of available commands:",
+      color: 413059,
+      fields: [
+        { name: "General use", value: "`/help` `/dq simple` `/dq poll`" },
+        { name: "Mod use only", value: "`/dqsend` `/queue` `/updatenum`" },
+      ],
+    };
+
+    await interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
+  }
+};
